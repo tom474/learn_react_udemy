7c710089bfa3fa0fb395f9b764606cb78d008187
Cuong Tran <emsignouc0407@gmail.com>
2024-10-18 17:20:23 +0700

    Complete Section 3

---
+++ 03_react_essentials/src/components/TabButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TabButton({ children, onSelect, isSelected }) {
+	return (
+		<li>
+			<button className={isSelected ? "active" : ""} onClick={onSelect}>
+				{children}
+			</button>
+		</li>
+	);
+}
